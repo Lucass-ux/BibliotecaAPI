a2f155da17a5bdbaa45c63e35ca3f78d87ca206d
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2026-03-03 15:34:34 -0300

    adicionando controllers

---
+++ APIBiblioteca/Controllers/GeneroController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIBiblioteca.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GeneroController : ControllerBase
+{
+}
