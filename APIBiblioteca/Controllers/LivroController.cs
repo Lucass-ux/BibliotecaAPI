c25750a40c69d0f2445b75ba1f9b8eb38c8823dc
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2026-03-06 15:30:46 -0300

    métodos GET no  LivrosController

--- APIBiblioteca/Controllers/LivroController.cs
+++ APIBiblioteca/Controllers/LivroController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Biblioteca.Application.DTOs;
+using Biblioteca.Application.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APIBiblioteca.Controllers;
@@ -7,4 +9,34 @@ namespace APIBiblioteca.Controllers;
 [ApiController]
 public class LivroController : ControllerBase
 {
+    private readonly LivroService _livroService;
+    public LivroController(LivroService livroService)
+    {
+        _livroService = livroService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<LivroDTO>>> Get()
+    {
+        var livros = await _livroService.GetAllAsync();
+        if(!livros.Any())
+            return NotFound("Livros não encontrados!");
+        return Ok(livros);
+    }
+    [HttpGet("{id:int:min(1)}", Name ="ObterLivro")]
+    public async Task<ActionResult<LivroDTO>> Get(int id)
+    {
+        if (id == null || id <= 0)
+            return BadRequest($"Id de livro inválido!");
+
+        var livro = _livroService.GetLivroAsync(id);
+
+        if (livro is null)
+            return NotFound("Livro de Id = {id} não encontrado!");
+
+        return Ok(livro);
+    }
+
+
+
 }
