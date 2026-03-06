f913209d8405cd42d77398c121f0b5adf837096e
Lucas Loiolla Gomes <loiollalucas@gmail.com>
2026-03-06 16:17:59 -0300

    configurando Swagger

--- APIBiblioteca/Program.cs
+++ APIBiblioteca/Program.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using Biblioteca.Application.DTOs.Mappings;
+using Biblioteca.Application.Services;
 using Biblioteca.Domain.Interfaces;
 using Biblioteca.Infraestructure.Context;
 using Biblioteca.Infraestructure.Repositories;
@@ -8,25 +11,47 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//
+
+//AppDbContext
 string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
                     options.UseMySql(mySqlConnection,
                     ServerVersion.AutoDetect(mySqlConnection)));
+//
+
+//AutoMapper
+
+builder.Services.AddSingleton<IMapper>(sp =>
+{
+    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.AddProfile<EntitiesDTOMappingProfile>();
+    }, loggerFactory);
+
+    return config.CreateMapper();
+});
+//
 
 builder.Services.AddScoped<ILivroRepository, LivroRepository>();
 builder.Services.AddScoped<IGeneroRepository, GeneroRepository>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<LivroService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
